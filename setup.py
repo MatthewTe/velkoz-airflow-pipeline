61c86be67602d94dd252e2a8f9bf275e8508cfbb
MatthewTee <teelucksingh.matthew1@gmail.com>
2020-11-18 12:09:26 -0400

    Added setup.py to package pipeline api

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "Velkoz Airflow Pipeline API",
+    url = "https://github.com/MatthewTe/velkoz_airflow_pipeline.git",
+    author = "Matthew Teelucksingh",
+    packages = ["velkoz_airflow_pipeline"],
+    license = 'MIT',
+    long_description=open('README.txt').read()   
+    )
