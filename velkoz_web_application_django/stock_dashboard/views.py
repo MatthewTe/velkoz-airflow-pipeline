31c79ca6d920168d77dd4ff1d94d34e8ff3a26ce
MatthewTee <teelucksingh.matthew1@gmail.com>
2020-11-24 12:39:42 -0400

    Barebones data querying for stock data dashboards of summary data

--- velkoz_web_application_django/stock_dashboard/views.py
+++ velkoz_web_application_django/stock_dashboard/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
 
+# Importing database model from the dashboard_core app:
+from dashboard_core.models import ExternalDatabase
+
+# Importing Velkoz Stock Data API:
+from velkoz_api.velkoz_query_api.velkoz_stock_data.stock_data_api import StockDataAPI
+
+# Importing 3rd Party Packages:
+import pandas as pd
+import json
 
 def stock_dashboard_index(request):
     """
-    """
-    return render(request, 'stock_dashboard/stock_dashboard_index.html')
+    TODO: Add Documentation.
+    """    
+    # Creating empty context to be passed into the html template:
+    context = {}
+    
+    # Querying the stock database model from the dashboard_core application:
+    stock_db = ExternalDatabase.objects.get(db_name = "stock_data_db")
+    stock_db_uri = stock_db.build_db_uri()
+
+    # Creating instance of Velkoz Data API to query data:
+    stock_data_api = StockDataAPI() # Database URI should be declared as environment variable.
+
+    # Querying external database for summary data on stock database:
+    stock_summary_df = stock_data_api.get_stock_database_summary()
+    
+    # Converting the dataframe into a json to be passed into the template:
+    json_stock_summary_data = stock_summary_df.reset_index().to_json(orient="records")
+    stock_summary_data = json.loads(json_stock_summary_data)
+
+    # Populating context:
+    context["stock_summary_table"] = stock_summary_data
+
+    return render(request, 'stock_dashboard/stock_dashboard_index.html', context=context)
